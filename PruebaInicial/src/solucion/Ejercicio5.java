c2848132be9a74dc38f5ddea9e17cdc3209031da
Juan Antonio Cuello <juanantonio.cuello@murciaeduca.es>
2026-03-20 16:58:34 +0100

    viernes

--- PruebaInicial/src/solucion/Ejercicio5.java
+++ PruebaInicial/src/solucion/Ejercicio5.java
@@ -22,21 +22,89 @@ public class Ejercicio5 {
         
         Scanner sc = new Scanner(System.in);
 
-        int num = 1;
-
-        while (num != 0) {
-
-            System.out.print("Introduce un número (0 para salir): ");
-            num = sc.nextInt();
-
-            if (num > 0) {
-                System.out.println("El número es positivo");
-            } 
-            else if (num < 0) {
-                System.out.println("El número es negativo");
+        int num;
+        
+        System.out.print("Dime un numero:");
+        num = sc.nextInt();
+        
+        while (num != 0 ){
+            if (num>0){
+                System.out.println("  El numero "+num+" es positivo");
             }
-
+            else{
+                System.out.println("  El numero "+num+" es negativo");
+            }
+            
+            System.out.print("Dime un numero:");
+            num = sc.nextInt();
         }
+        
+        System.out.println("FIN");        
+        
+        
+        
+        //VERSION CON while(true)
+        //No se debe hacer
+//        while (true){
+//            System.out.print("Dime un numero:");
+//            num = sc.nextInt();
+//            
+//            if(num==0){
+//                break;
+//            }
+//            
+//            if (num>0){
+//                System.out.println("  El numero "+num+" es positivo");
+//            }
+//            else{
+//                System.out.println("  El numero "+num+" es negativo");
+//            }
+//        }
+        
+        
+        
+//        num = 1;
+//
+//        while (num != 0) {
+//
+//            System.out.print("Introduce un número (0 para salir): ");
+//            num = sc.nextInt();
+//
+//            if (num > 0) {
+//                System.out.println("El número es positivo");
+//            } 
+//            else if (num < 0) {
+//                System.out.println("El número es negativo");
+//            }
+//
+//        }
+        
+        
+//          do{
+//            
+//            System.out.print("Introduce un número (0 para salir): ");
+//            num = sc.nextInt();
+//            
+//
+//            if (num > 0) {
+//                System.out.println("El número "+num+" es positivo");
+//            } 
+//            else if(num < 0){
+//                System.out.println("El número "+num+" es negativo");
+//            }
+//              
+//              
+//          }while(num!=0);  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         sc.close();
         
